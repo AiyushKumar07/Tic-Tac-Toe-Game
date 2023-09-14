4652aba23c27084a8e39c0d14f9e7dceceaaff83
Aiyush Kumar <aiyushkanojia@gmail.com>
2023-09-14 12:58:01 +0530

    Updated Status_message

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import './styles.scss';
 import { useState } from 'react';
 import Board from './components/Board';
 import { calculateWinner } from './winner';
+import StatusMessage from './components/StatusMessage';
 
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setXNext] = useState(false);
   const winner = calculateWinner(squares);
-  const nextPlayer = isXNext ? 'X' : 'O';
-  const statusMessage = winner
-    ? `Winner is ${winner}`
-    : `Next Player is ${nextPlayer}`;
 
   const handleSquareClick = clickedPosition => {
     if (squares[clickedPosition] || winner) {
@@ -29,7 +26,7 @@ function App() {
   };
   return (
     <div className="app">
-      <h2>{statusMessage}</h2>
+      <StatusMessage winner={winner} isXNext={isXNext} squares={squares} />
       <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
